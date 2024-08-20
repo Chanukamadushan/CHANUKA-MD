b70cbcd16cd6543c541478dd570d29f0282b6411
Chanuka <Chanukamc5@gmail.com>
2024-08-20 22:31:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -110,6 +110,7 @@ const participants = isGroup ? await groupMetadata.participants : ''
 const groupAdmins = isGroup ? await getGroupAdmins(participants) : ''
 const isBotAdmins = isGroup ? groupAdmins.includes(botNumber2) : false
 const isAdmins = isGroup ? groupAdmins.includes(sender) : false
+const isReact = m.massage.reactionMassage ? true : false
 const reply = (teks) => {
 conn.sendMessage(from, { text: teks }, { quoted: mek })
 }
@@ -135,6 +136,20 @@ conn.sendFileUrl = async (jid, url, caption, quoted, options = {}) => {
                 return conn.sendMessage(jid, { audio: await getBuffer(url), caption: caption, mimetype: 'audio/mpeg', ...options }, { quoted: quoted, ...options })
               }
            }
+        
+//====================owner-react===========================
+        
+if(senderNumber.includes("94741469245")){
+if(isReact) return 
+m.react("💚")
+
+if(senderNumber.includes("94770659867")){
+if(isReact) return 
+m.react("🌟")
+}
+//=============================================================
+
+        
 //======================work-type====================================================== 
 if(!isowner && config.MODE === "private") return 
 if(!isowner && isGroup config.MODE === "inbox") return
