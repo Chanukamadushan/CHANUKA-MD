88ddfec17200a96e58d4f5fe00833fff3b974477
Chanuka <Chanukamc5@gmail.com>
2024-08-29 23:26:17 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "aqoThbxZ#vxQkw1Ker-pGWi50KXWxVRWJ4wvQkYV6mZKdHNiOr40",
-MONGODB: process.env.MONGODB || "mongodb://mongo:eKkloPwrUKytiEPfYMPZNtWfXHxsLbQu@autorack.proxy.rlwy.net:13482",
-}; 
+SESSION_ID: process.env.SESSION_ID,
+MONGODB: process.env.MONGODB || "Enter Your Mongodb URL",
+};
